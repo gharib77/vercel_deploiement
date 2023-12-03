c26888dea53389ff3da5ba9c4392b946b2ae2cdb
unknown <gharibjouali@gmail.com>
2023-12-03 15:19:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+const port = 5002 || process.env.PORT
+
+app.use(express.json())
+app.get("/",(req,res)=>{
+  return res.send("je suis la")
+})
+
+app.listen(port,()=>console.log("server is runing...."))
