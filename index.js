22dd047565cc609f4e825e733d876562515a37f3
unknown <gharibjouali@gmail.com>
2023-12-03 15:22:01 +0100

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 5002 || process.env.PORT
 
 app.use(express.json())
 app.get("/",(req,res)=>{
-  return res.send("je suis la")
+  return res.send("je suis la mes amis")
 })
 
 app.listen(port,()=>console.log("server is runing...."))
